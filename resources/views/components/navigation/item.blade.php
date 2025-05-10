1a3e9f6a318da51e1adc8d37541bf8404221f85f
Mika van Os <os0017@hz.nl>
2025-05-10 17:41:15 +0200

    Testing something silly

--- resources/views/components/navigation/item.blade.php
+++ resources/views/components/navigation/item.blade.php
@@ -1,7 +1,7 @@
 <li class="mr-3">
     <a href="{{ route($route) }}" class="inline-block border {{ Request::url() == route($route) ?
-'border-blue-500 rounded py-1 px-3 bg-blue-500 text-white' :
- 'border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3'
+'border-purple-500 rounded py-1 px-3 bg-purple-500 text-white' :
+ 'border-white rounded hover:border-gray-200 text-purple-500 hover:bg-gray-200 py-1 px-3'
  }}">
         {{ $slot }}
     </a>
